9961a636b00d01acc1c8c493623988dd7fd11c32
Antonin Gallot <antonin.gallot@a-018818.ups.u-psud.fr>
2019-03-14 12:28:39 +0100

    ça marche pas mais y'a de l'idée

--- tp05/init.js
+++ tp05/init.js
@@ -1,5 +1,14 @@
 window.addEventListener("load", function () {
     let search = document.getElementById("search");
     let searchButton = document.getElementById("searchButton");
-    /**** À COMPLÉTER ****/
+    let searchResult = document.getElementById("searchResults");
+    
+    searchButton.addEventListener("click", function(){
+        console.log("EventListener");
+        let res = MediaWiki.searchImages(search.textContent);
+        for (elem in res) {
+            let img = elem.query.pages.title;
+            searchResult.innerHTML += "'<img src = " + img + "/>'";
+        }
+    })
 });
